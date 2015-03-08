619199af5ec7ec78580fd6b748bcba4ed0c6f6ba
Chen Zihui <hello@chenzihui.com>
2015-03-08 23:04:58 +0800

    Add tokens via textarea

--- example/app.js
+++ example/app.js
@@ -18418,75 +18418,89 @@ module.exports = React.createClass({
   },
 
   componentDidMount: function componentDidMount() {
-    this.getDOMNode().focus();
+    this.refs.tokenInput.getDOMNode().focus();
   },
 
-  componentDidUpdate: function componentDidUpdate() {
-    var node = this.getDOMNode(),
-        children = node.childNodes,
-        lastNode = children[children.length - 1];
+  // componentDidUpdate() {
+  //   let node     = this.getDOMNode(),
+  //       children = node.childNodes,
+  //       lastNode = children[children.length - 1];
 
-    if (this.props.tokens.length > 0) {
-      if (lastNode.nodeType !== 3 || lastNode.textContent.trim()) {
-        node.innerHTML += "&nbsp;";
-      }
-      _setCaretAtEnd(node);
-    } else {
-      node.innerHTML = "";
-    }
-  },
+  //   if (this.props.tokens.length > 0) {
+  //     if (lastNode.nodeType !== 3 || lastNode.textContent.trim()) {
+  //       node.innerHTML += '&nbsp;';
+  //     }
+  //     _setCaretAtEnd(node);
+  //   } else {
+  //     node.innerHTML = '';
+  //   }
+  // },
 
   render: function render() {
-    var items = this.props.tokens.map(function (token, index) {
+    var tokens = this.props.tokens.map(function (token, index) {
       return React.createElement(TokenCell, { key: index, textContent: token });
     });
 
     return React.createElement(
       "div",
-      { className: "rt-tokenizer",
-        contentEditable: "true",
-        onKeyDown: this._handleKeyDown,
-        onPaste: this._handlePaste },
-      items
+      { className: "rt-tokenizer" },
+      tokens,
+      React.createElement("textarea", {
+        className: "rt-tokenizer__user-input",
+        ref: "tokenInput",
+        onKeyDown: this._handleKeyDown })
     );
   },
 
   _handleKeyDown: function _handleKeyDown(evt) {
-    var node = this.getDOMNode(),
-        children = node.childNodes,
-        lastNode = undefined,
-        textContent = undefined;
+    var userInput = this.refs.tokenInput.getDOMNode(),
+        token = userInput.value;
 
     if (SEPERATORS.indexOf(evt.which) !== -1) {
       evt.preventDefault();
 
-      if (children.length > 0) {
-        lastNode = children[children.length - 1];
-        textContent = lastNode.textContent.trim();
-
-        if (lastNode.nodeType === 3 && textContent) {
-          node.removeChild(lastNode);
-          this.props.tokenize(textContent);
-        }
-      }
-    } else if (evt.which === KEYS.BACKSPACE) {
-      var anchorNode = undefined,
-          prevSibling = undefined;
-
-      if (children.length > 0) {
-        lastNode = children[children.length - 1];
-
-        if (!lastNode.textContent.trim()) {
-          evt.preventDefault();
-
-          anchorNode = window.getSelection().anchorNode;
-          prevSibling = anchorNode.previousSibling;
-
-          this.props.removeToken(prevSibling.textContent.trim());
-        }
+      if (token.trim()) {
+        this.props.tokenize(token);
+        userInput.value = "";
       }
     }
   },
+
+  // _handleKeyDown(evt) {
+  //   let node     = this.getDOMNode(),
+  //       children = node.childNodes,
+
+  //       lastNode, textContent;
+
+  //   if (SEPERATORS.indexOf(evt.which) !== -1) {
+  //     evt.preventDefault();
+
+  //     if (children.length > 0) {
+  //       lastNode    = children[children.length - 1];
+  //       textContent = lastNode.textContent.trim();
+
+  //       if (lastNode.nodeType === 3 && textContent) {
+  //         node.removeChild(lastNode);
+  //         this.props.tokenize(textContent);
+  //       }
+  //     }
+  //   } else if (evt.which === KEYS.BACKSPACE) {
+  //     let anchorNode, prevSibling;
+
+  //     if (children.length > 0) {
+  //       lastNode = children[children.length - 1];
+
+  //       if (!lastNode.textContent.trim()) {
+  //         evt.preventDefault();
+
+  //         anchorNode = window.getSelection().anchorNode;
+  //         prevSibling = anchorNode.previousSibling;
+
+  //         this.props.removeToken(prevSibling.textContent.trim());
+  //       }
+  //     }
+  //   }
+  // },
 
   _handlePaste: function _handlePaste(evt) {
     evt.preventDefault();
--- src/Tokenizer.js
+++ src/Tokenizer.js
@@ -38,72 +38,90 @@ export default React.createClass({
   },
 
   componentDidMount() {
-    this.getDOMNode().focus();
+    this.refs.tokenInput.getDOMNode().focus();
   },
 
-  componentDidUpdate() {
-    let node     = this.getDOMNode(),
-        children = node.childNodes,
-        lastNode = children[children.length - 1];
+  // componentDidUpdate() {
+  //   let node     = this.getDOMNode(),
+  //       children = node.childNodes,
+  //       lastNode = children[children.length - 1];
 
-    if (this.props.tokens.length > 0) {
-      if (lastNode.nodeType !== 3 || lastNode.textContent.trim()) {
-        node.innerHTML += '&nbsp;';
-      }
-      _setCaretAtEnd(node);
-    } else {
-      node.innerHTML = '';
-    }
-  },
+  //   if (this.props.tokens.length > 0) {
+  //     if (lastNode.nodeType !== 3 || lastNode.textContent.trim()) {
+  //       node.innerHTML += '&nbsp;';
+  //     }
+  //     _setCaretAtEnd(node);
+  //   } else {
+  //     node.innerHTML = '';
+  //   }
+  // },
 
   render() {
-    let items = this.props.tokens.map(function(token, index) {
+    let tokens = this.props.tokens.map(function(token, index) {
       return <TokenCell key={index} textContent={token} />;
     });
 
     return (
-      <div className="rt-tokenizer"
-        contentEditable="true"
-        onKeyDown={this._handleKeyDown}
-        onPaste={this._handlePaste}>{items}</div>
+      <div className="rt-tokenizer">
+        {tokens}
+
+        <textarea
+          className="rt-tokenizer__user-input"
+          ref="tokenInput"
+          onKeyDown={this._handleKeyDown}></textarea>
+      </div>
     );
   },
 
   _handleKeyDown(evt) {
-    let node     = this.getDOMNode(),
-        children = node.childNodes,
-
-        lastNode, textContent;
+    let userInput = this.refs.tokenInput.getDOMNode(),
+        token     = userInput.value;
 
     if (SEPERATORS.indexOf(evt.which) !== -1) {
       evt.preventDefault();
 
-      if (children.length > 0) {
-        lastNode    = children[children.length - 1];
-        textContent = lastNode.textContent.trim();
-
-        if (lastNode.nodeType === 3 && textContent) {
-          node.removeChild(lastNode);
-          this.props.tokenize(textContent);
-        }
-      }
-    } else if (evt.which === KEYS.BACKSPACE) {
-      let anchorNode, prevSibling;
-
-      if (children.length > 0) {
-        lastNode = children[children.length - 1];
-
-        if (!lastNode.textContent.trim()) {
-          evt.preventDefault();
-
-          anchorNode = window.getSelection().anchorNode;
-          prevSibling = anchorNode.previousSibling;
-
-          this.props.removeToken(prevSibling.textContent.trim());
-        }
+      if (token.trim()) {
+        this.props.tokenize(token);
+        userInput.value = '';
       }
     }
   },
+
+  // _handleKeyDown(evt) {
+  //   let node     = this.getDOMNode(),
+  //       children = node.childNodes,
+
+  //       lastNode, textContent;
+
+  //   if (SEPERATORS.indexOf(evt.which) !== -1) {
+  //     evt.preventDefault();
+
+  //     if (children.length > 0) {
+  //       lastNode    = children[children.length - 1];
+  //       textContent = lastNode.textContent.trim();
+
+  //       if (lastNode.nodeType === 3 && textContent) {
+  //         node.removeChild(lastNode);
+  //         this.props.tokenize(textContent);
+  //       }
+  //     }
+  //   } else if (evt.which === KEYS.BACKSPACE) {
+  //     let anchorNode, prevSibling;
+
+  //     if (children.length > 0) {
+  //       lastNode = children[children.length - 1];
+
+  //       if (!lastNode.textContent.trim()) {
+  //         evt.preventDefault();
+
+  //         anchorNode = window.getSelection().anchorNode;
+  //         prevSibling = anchorNode.previousSibling;
+
+  //         this.props.removeToken(prevSibling.textContent.trim());
+  //       }
+  //     }
+  //   }
+  // },
 
   _handlePaste(evt) {
     evt.preventDefault();
